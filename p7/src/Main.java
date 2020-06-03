3ccaa6bec26c900071213711d70849b5da615490
SoheilNamdar <soheilnamdar@yahoo.com>
2020-06-03 16:15:53 +0000

    Directive factorial

--- p7/src/Main.java
+++ p7/src/Main.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int number = 5;
-        int result = recursiveFactorial(number);
+        int result = directiveFactorial(number);
         System.out.println(result);
     }
 
-   /* public static int directiveFactorial(int number) {
+    public static int directiveFactorial(int number) {
         int result = 1;
-        for (int i=5; i>=1 ; i--) {
+        for (int i=number; i>=1 ; i--) {
             result *= i;
         }
         return result;
-    }*/
+    }
 
-   public static int recursiveFactorial(int number) {
+   /*public static int recursiveFactorial(int number) {
        if(number == 0)
            return 1;
        else
         return number * recursiveFactorial(number-1);
-   }
+   }*/
 }
