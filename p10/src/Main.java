1965cb5d9aab56741665f073e32abc2665ae527a
SoheilNamdar <soheilnamdar@yahoo.com>
2020-06-04 07:11:35 +0000

    directiveFibonacci

--- p10/src/Main.java
+++ p10/src/Main.java
@@ -2,15 +2,27 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 3;
-        int result = fibonacci(number);
-        System.out.println(result);
+        int number = 31;
+        for(int i=1 ; i<=number ; i++){
+           directiveFibonacci(i);
+        }
     }
 
-    public static int fibonacci(int number) {
-        while(number>1)
-            return fibonacci(number - 1) * fibonacci(number - 2);
-        else
-            return 1;
+    public static void directiveFibonacci(int number){
+        int n1 = 1, n2 = 1, n3;
+        for(int i=1 ; i<=number ; i++){
+            n3 = n1 + n2;
+            System.out.println(n3);
+            n1 = n2;
+            n2 = n3;
+        }
     }
+
+    /*public static int recursiveFibonacci(int number) {
+        if (number == 0 || number == 1)
+            return 1;
+        else {
+                return fibonacci(number - 1) + fibonacci(number - 2);
+        }
+    }*/
 }
