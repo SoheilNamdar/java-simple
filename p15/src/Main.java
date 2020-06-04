11a5e4a6dc751beccd70be4934db33c183b7d422
SoheilNamdar <soheilnamdar@yahoo.com>
2020-06-04 09:06:48 +0000

    sub App

--- p15/src/Main.java
+++ p15/src/Main.java
@@ -3,13 +3,25 @@ public class Main {
         String s = "ADcvADcvmlokjQSQDRF", result = "";
         String s1 = "";
         for (int i = 0; i < s.length() ; i++) {
-            s1 +=s.charAt(i);
+          //  s1 +=s.charAt(i);
             for (int j = i; j < s.length() ; j++) {
-                s1 +=s.charAt(j);
+               s1 =subString(s, i, j+1);
+                //s1 =s.substring(i,j+1);
             }
-            System.out.println("#" + i +  " :" + s);
+            System.out.println("#" + i +  " :" + s1);
             s1 = "";
         }
 
+
+
+    }
+    public static String subString(String input, int index1, int index2){
+        //String s = "ADcvADcvmlokjQSQDRF",
+        String result = "";
+        int n1=4, n2=9;
+        for (int i = index1; i < input.length() && i<index2; i++) {
+            result += input.charAt(i);
+        }
+        return result;
     }
 }
